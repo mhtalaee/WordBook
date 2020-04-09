71e8f09f2f8acc1190edc4fd5ae0d1bc2e58cb7d
mhtalaee <mhtalaee@gmail.com>
2020-04-09 22:36:26 +0430

    initial commit

--- app/src/main/java/ir/goldenmind/wordbook/utils/Converters.kt
+++ app/src/main/java/ir/goldenmind/wordbook/utils/Converters.kt
@@ -4,6 +4,8 @@ import androidx.room.TypeConverter
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.format.DateTimeFormatter
 
+// This class is used to convert LocalDateTime to String because Room (Sqlite)
+//doesn't know LocalDateTime (Then doesn't know any date type) and store it as String (or Long)
 class Converters {
 
     private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
